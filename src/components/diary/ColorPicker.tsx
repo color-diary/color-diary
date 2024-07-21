a6c97d15324e10819924af2fe54196718cb6ffed
hyejin <po852alth@naver.com>
2024-07-21 13:35:19 +0900

    update: 컬러피커 기능완료

---
+++ src/components/diary/ColorPicker.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import React, { useState } from 'react';
+
+const ColorPicker = () => {
+  const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+  const [customColor, setCustomColor] = useState('');
+  const [showRainbow, setShowRainbow] = useState(true);
+  const [pickedColor, setPickedColor] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const color = event.target.value;
+    setCustomColor(color);
+    setPickedColor(color);
+    setShowRainbow(false);
+  };
+
+  const handleRainbowClick = () => {
+    setShowRainbow(true);
+  };
+
+  const handleColor = (color: string) => {
+    setPickedColor(color);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <p>오늘의 색은 무엇인가요?</p>
+        <div className="flex space-x-2 items-center">
+          <br />
+          {colors.map((color) => (
+            <div
+              key={color}
+              className={`w-[40px] h-[40px] rounded-full cursor-pointer bg-${color} ${
+                pickedColor === color ? 'border-4 border-black' : ''
+              }`}
+              onClick={() => {
+                handleColor(color);
+              }}
+            />
+          ))}
+          {showRainbow ? (
+            <div className="relative w-[40px] h-[40px] rounded-full overflow-hidden" onClick={handleRainbowClick}>
+              <input
+                type="color"
+                value={customColor}
+                onChange={handleChange}
+                className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+              />
+              <div className="w-[40px] h-[40px] rounded-full rainbow-gradient"></div>
+            </div>
+          ) : (
+            <div className="relative w-[40px] h-[40px] rounded-full overflow-hidden">
+              <input
+                type="color"
+                value={customColor}
+                onChange={handleChange}
+                className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+              />
+              <div
+                className={`w-[40px] h-[40px] rounded-full cursor-pointer bg-${customColor} ${
+                  pickedColor === customColor ? 'border-4 border-black' : ''
+                }`}
+              ></div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ColorPicker;
