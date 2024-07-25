7648e55ea976733c98928c6521470e5562ca2607
hyejin <po852alth@naver.com>
2024-07-25 11:52:02 +0900

    fix: write,edit page notfound 처리 수정

--- src/app/diaries/edit/[id]/page.tsx
+++ src/app/diaries/edit/[id]/page.tsx
@@ -1,21 +1,6 @@
 import WriteForm from '@/components/diary/WriteForm';
+import { isValidUUID } from '@/utils/paramsValidation';
 import { notFound } from 'next/navigation';
-import { validate as uuidValidate } from 'uuid';
-
-const isValidUUID = (id: string) => {
-  return uuidValidate(id);
-};
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  if (!isValidUUID(params.id)) {
-    return {
-      title: '404 Not Found'
-    };
-  }
-  return {
-    title: 'Edit Diary'
-  };
-}
 
 export default function EditPage({ params }: { params: { id: string } }) {
   if (!isValidUUID(params.id)) {
--- src/app/diaries/write/[id]/page.tsx
+++ src/app/diaries/write/[id]/page.tsx
@@ -1,21 +1,6 @@
 import WriteForm from '@/components/diary/WriteForm';
+import { isValidDate } from '@/utils/paramsValidation';
 import { notFound } from 'next/navigation';
-
-const isValidDate = (id: string) => {
-  const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-  return datePattern.test(id);
-};
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  if (!isValidDate(params.id)) {
-    return {
-      title: '404 Not Found'
-    };
-  }
-  return {
-    title: 'Write Diary'
-  };
-}
 
 export default function WritePage({ params }: { params: { id: string } }) {
   if (!isValidDate(params.id)) {
