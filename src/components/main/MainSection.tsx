9fa9d9eacad9b215ddccd15a7f38e77a58e1602f
Cameron <lyle.usn@gmail.com>
2024-07-31 14:19:23 +0900

    update: 카드형태에서 작성된게 없을 경우, 작성 순서가 아니라 날짜 순으로 정렬 , 미래날자 작성불가, caption label 형식변경, 날짜이동 캘린더 추가

--- src/components/main/MainSection.tsx
+++ src/components/main/MainSection.tsx
@@ -1,28 +1,37 @@
 'use client';
 
 import React from 'react';
-import MainCalendar from '@/components/main/MainCalendar';
 import Cards from '@/components/main/Cards';
 import { useRouter, useSearchParams } from 'next/navigation';
 import axios from 'axios';
 import { DiaryList } from '@/types/diary.type';
 import { createClient } from '@/utils/supabase/client';
+import CalendarLibrary from './CalendarLibrary';
 
 const MainSection = () => {
-  const router = useRouter();
   const now = new Date();
+  const router = useRouter();
   const params = useSearchParams();
   const isCards = params.get('form') === 'cards';
   const [form, setForm] = React.useState(isCards ? 'cards' : 'calendar');
-  const [diaryList, setDiaryList] = React.useState<DiaryList>([]);
-  const [year, setYear] = React.useState<number>(now.getFullYear());
-  const [month, setMonth] = React.useState<number>(now.getMonth() + 1);
+  const [diaryList, setDiaryList] = React.useState<DiaryList | null>(null);
   const [queryString, setQueryString] = React.useState<String>();
-  const [date, setDate] = React.useState<Date>(now);
-  //
-  // params get yymm ? 거기서 잘라서 넣기 : now.getFullYear()
-  // month도 해라
 
+  const handleYearMonthDefaultValue = (type: string) => {
+    const YYMM = params.get('YYMM');
+    if (YYMM) {
+      if (type === 'year') {
+        return YYMM.slice(0, 4);
+      }
+      if (type === 'month') {
+        return YYMM.slice(4, 6);
+      }
+    }
+  };
+
+  const [year, setYear] = React.useState<number>(Number(handleYearMonthDefaultValue('year')));
+  const [month, setMonth] = React.useState<number>(Number(handleYearMonthDefaultValue('month')));
+  const [date, setDate] = React.useState<Date>(now);
   const handleClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     const { name } = e.target as HTMLButtonElement;
     if (name === form) {
@@ -34,20 +43,21 @@ const MainSection = () => {
   // let current = document.getElementById(currentClick);
   // current.style.color = 'black';
 
-  // for (const [key, value] of params) {
-  //   console.log(`${key}: ${value}`);
-  // }
-
   const handleQueryString = () => {
-    if (String(month).length === 1) {
-      setQueryString(`?form=${form}&YYMM=${String(year) + String(0) + String(month)}`);
+    if (!year) {
+      setYear(now.getFullYear());
+      setMonth(now.getMonth() + 1);
     } else {
-      setQueryString(`?form=${form}&YYMM=${String(year) + String(month)}`);
+      if (String(month).length === 1) {
+        setQueryString(`?form=${form}&YYMM=${String(year) + String(0) + String(month)}`);
+      } else {
+        setQueryString(`?form=${form}&YYMM=${String(year) + String(month)}`);
+      }
     }
   };
 
-  const handleInputChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const date = new Date(event.target.value);
+  const handleInputChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const date = new Date(e.target.value);
 
     if (date) {
       setYear(date.getFullYear());
@@ -55,24 +65,23 @@ const MainSection = () => {
       setDate(date);
     }
   };
-  console.log(queryString);
 
-  // 오늘 이후의 날짜면 클릭 못함
-  //
   const getDiaryList = async () => {
-    try {
-      const supabase = createClient();
-      const {
-        data: { session },
-        error
-      } = await supabase.auth.getSession();
+    if (year) {
+      try {
+        const supabase = createClient();
+        const {
+          data: { session },
+          error
+        } = await supabase.auth.getSession();
 
-      if (error) {
-        throw new Error(error.message);
+        if (error) {
+          throw new Error(error.message);
+        }
+        session ? getUserDiaryList() : getUserLocalList();
+      } catch (error) {
+        console.error('Failed to get session:', error);
       }
-      session ? getUserDiaryList() : getUserLocalList();
-    } catch (error) {
-      console.error('Failed to get session:', error);
     }
   };
 
@@ -80,7 +89,10 @@ const MainSection = () => {
     const response = await axios.get('/api/diaries', {
       params: { year, month }
     });
-    setDiaryList(response.data);
+    const data = response.data;
+    if (data) {
+      setDiaryList(data);
+    }
   };
 
   const getUserLocalList = async () => {
@@ -100,13 +112,19 @@ const MainSection = () => {
   React.useEffect(() => {
     handleQueryString();
   }, [form, year, month]);
-  console.log(year, month);
 
   React.useEffect(() => {
     if (queryString) {
-      router.replace(`${queryString}`);
+      router.push(`${queryString}`);
     }
   }, [queryString]);
+
+  React.useEffect(() => {
+    const a = handleYearMonthDefaultValue('year');
+    const b = handleYearMonthDefaultValue('month');
+
+    setDate(new Date(Number(a), Number(b) - 1, 1));
+  }, [params]);
 
   return (
     <div className="flex flex-col mx-auto mt-[8rem] w-[46.5rem]">
@@ -134,12 +152,19 @@ const MainSection = () => {
       </div>
       <div>
         {isCards ? (
-          <Cards diaryList={diaryList} />
+          <Cards
+            diaryList={diaryList}
+            isCards={isCards}
+            date={date}
+            setDate={setDate}
+            handleInputChangeDate={handleInputChangeDate}
+          />
         ) : (
-          <MainCalendar
+          <CalendarLibrary
             diaryList={diaryList}
             date={date}
             setDate={setDate}
+            isCards={isCards}
             handleInputChangeDate={handleInputChangeDate}
           />
         )}
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,18 +3,29 @@
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
-
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
 import Stamp from '../main/Stamp';
 import { useRouter } from 'next/navigation';
 import { formatFullDate } from '@/utils/dateUtils';
 import { Diary, DiaryList } from '@/types/diary.type';
 import { createClient } from '@/utils/supabase/client';
+import '../main/dateInput.css';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker> & { diaryList: DiaryList };
+export type CalendarProps = React.ComponentProps<typeof DayPicker> & {
+  diaryList: DiaryList;
+  isCards: boolean;
+  handleInputChangeDate?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-function Calendar({ className, classNames, showOutsideDays = false, diaryList, ...props }: CalendarProps) {
+function Calendar({
+  className,
+  classNames,
+  showOutsideDays = false,
+  diaryList,
+  isCards,
+  handleInputChangeDate,
+  ...props
+}: CalendarProps) {
   const route = useRouter();
   const today = new Date();
   return (
@@ -22,19 +33,16 @@ function Calendar({ className, classNames, showOutsideDays = false, diaryList, .
       showOutsideDays={showOutsideDays}
       className={cn('p-3', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 ',
+        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4',
         caption: 'flex justify-center pt-1 relative items-center',
         caption_label: 'text-sm font-medium',
         nav: 'space-x-1 flex items-center',
-        nav_button: cn(
-          buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
-        ),
+        nav_button: cn('h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'),
         nav_button_previous: 'absolute left-1',
         nav_button_next: 'absolute right-1',
         table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
+        head_row: `${isCards ? 'hidden' : 'flex'}`,
         head_cell: 'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
         row: 'flex w-full mt-2',
         cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
@@ -53,18 +61,30 @@ function Calendar({ className, classNames, showOutsideDays = false, diaryList, .
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-        DayContent: ({ ...props }) => {
-          // param.get보다는 form(isCards)를 props로 받아서 삼항연산자
+        CaptionLabel: ({ ...props }) => {
+          const dateInputRef = React.useRef<HTMLInputElement>(null);
+          const handleInputDateChange = () => {
+            if (dateInputRef.current) {
+              dateInputRef.current.showPicker();
+            }
+          };
 
-          //const [isToday, setIsToday] = React.useState(false);
-          // let isToday = false;
-          //formatFullDate(String(today)) === formatFullDate(String(props.date))
+          return (
+            <div className="anchor">
+              <input type="date" ref={dateInputRef} style={{ visibility: 'hidden' }} onChange={handleInputChangeDate} />
+              <p onClick={() => handleInputDateChange()}>
+                {props.displayMonth.getFullYear()}년 {props.displayMonth.getMonth() + 1}월
+              </p>
+            </div>
+          );
+        },
+        DayContent: ({ ...props }) => {
           let isToday = false;
           if (formatFullDate(String(today)) === formatFullDate(String(props.date))) {
             isToday = true;
           }
 
-          const checkNotLoggedin = async () => {
+          const handleGoWirtePage = async () => {
             try {
               const supabase = createClient();
               const {
@@ -77,16 +97,21 @@ function Calendar({ className, classNames, showOutsideDays = false, diaryList, .
               }
               if (!session) {
                 if (2 <= diaryList.length) {
-                  alert('비회원은 2개이상 못써용');
+                  alert('비회원은 2개이상 작성할 수 없습니다.');
                 } else {
                   route.push(`/diaries/write/${formatFullDate(String(props.date))}`);
                 }
-              } else route.push(`/diaries/write/${formatFullDate(String(props.date))}`);
+              } else {
+                if (today < props.date) {
+                  alert('미래의 일기는 작성하실 수 없습니다.');
+                } else {
+                  route.push(`/diaries/write/${formatFullDate(String(props.date))}`);
+                }
+              }
             } catch (error) {
               console.error('Failed to get session:', error);
             }
           };
-          console.log();
 
           const handleFindDiary = (diary: Diary) => {
             if (formatFullDate(String(diary.date)) === formatFullDate(String(props.date))) {
@@ -98,15 +123,21 @@ function Calendar({ className, classNames, showOutsideDays = false, diaryList, .
           const [diary, setDiary] = React.useState<Diary | undefined>();
 
           React.useEffect(() => {
-            setDiary(diaryList.find(handleFindDiary));
+            if (isCards) {
+              return;
+            } else {
+              setDiary(diaryList?.find(handleFindDiary));
+            }
           }, []);
 
-          return diary ? (
+          return isCards ? (
+            <></>
+          ) : diary ? (
             <div
               onClick={() => {
                 route.push(`/diaries/${diary.diaryId}`);
               }}
-              className="flex flex-col items-center "
+              className="flex flex-col items-center"
             >
               <Stamp petal={diary.color} circle="#F7CA87" month={props.date.getMonth() + 1} />
               <p className="text-sm">{props.date.getDate()}</p>
@@ -114,7 +145,7 @@ function Calendar({ className, classNames, showOutsideDays = false, diaryList, .
           ) : (
             <div
               onClick={() => {
-                checkNotLoggedin();
+                handleGoWirtePage();
               }}
               className="flex flex-col items-center "
             >
