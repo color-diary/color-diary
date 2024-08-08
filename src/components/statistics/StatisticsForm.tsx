d23f8b1f68458022d9234749671457c4c641bc9f
minseok <minskyson7@gmail.com>
2024-08-08 18:39:59 +0900

    style: advice font 적용

--- src/components/statistics/StatisticsForm.tsx
+++ src/components/statistics/StatisticsForm.tsx
@@ -14,8 +14,6 @@ const myFont = localFont({
 const StatisticsForm = () => {
   const [advice, setAdvice] = useState<KadviceJsonModel | null>(null);
 
-  console.log(myFont);
-
   useEffect(() => {
     const advice = kadvice.getOne();
     setAdvice(advice);
@@ -26,16 +24,13 @@ const StatisticsForm = () => {
   }
 
   return (
-    <div className="md:bg-[#FBF8F4] rounded-5xl w-744px-row h-916px-col flex items-center flex-col md:mt-128px-col mt-[94px] m-auto bg-[#FEFDFB] border-0 md:border-4 border-[#E6D3BC]">
+    <div className="md:bg-[#FBF8F4] rounded-5xl w-744px-row md:h-916px-col flex items-center flex-col md:mt-128px-col mt-[94px] m-auto bg-[#FEFDFB] border-0 md:border-4 border-[#E6D3BC]">
       <div className="md:w-600px-row md:h-401px-col w-[335px] h-[240px] md:mt-56px-col">
         <h2 className="md:text-24px text-[18px] font-bold text-font-color">이달의 감정 색상</h2>
         <div className="flex md:mt-8px-col mt-2">
           <ColorChart />
           <div
-            className={
-              'md:w-160px-row md:h-160px-col md:mt-105px-col md:ml-12px-row md:px-12px-row md:py-12px-col text-16px md:inline hidden text-[#545454] ' +
-              myFont.className
-            }
+            className={`md:w-160px-row md:h-160px-col md:mt-105px-col md:ml-12px-row md:px-12px-row md:py-12px-col text-16px md:inline hidden text-[#545454] ${myFont.className}`}
           >
             &quot;{advice.message}&quot;
           </div>
@@ -46,7 +41,7 @@ const StatisticsForm = () => {
         <EmotionChart />
       </div>
       <div
-        className={'md:hidden w-[330px] h-[44px] text-[16px] mt-[24px] text-center text-[#545454]' + myFont.className}
+        className={`md:hidden w-[330px] h-[44px] text-[16px] mt-[24px] text-center text-[#545454] ${myFont.className}`}
       >
         &quot;{advice.message}&quot;
       </div>
