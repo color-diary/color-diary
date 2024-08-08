bbd23138174d5e28fefbc5c673415a2420bfce5c
minseok <minskyson7@gmail.com>
2024-08-08 16:58:16 +0900

    style: advicefont

--- src/components/statistics/StatisticsForm.tsx
+++ src/components/statistics/StatisticsForm.tsx
@@ -4,9 +4,17 @@ import { kadvice, KadviceJsonModel } from 'kadvice';
 import { useEffect, useState } from 'react';
 import ColorChart from './ColorChart';
 import EmotionChart from './EmotionChart';
+import localFont from 'next/font/local';
+
+const myFont = localFont({
+  src: '../../fonts/BMJUA_otf.otf',
+  display: 'swap'
+});
 
 const StatisticsForm = () => {
   const [advice, setAdvice] = useState<KadviceJsonModel | null>(null);
+
+  console.log(myFont);
 
   useEffect(() => {
     const advice = kadvice.getOne();
@@ -23,8 +31,13 @@ const StatisticsForm = () => {
         <h2 className="md:text-24px text-[18px] font-bold text-font-color">이달의 감정 색상</h2>
         <div className="flex md:mt-8px-col mt-2">
           <ColorChart />
-          <div className="md:w-160px-row md:h-160px-col md:mt-105px-col md:ml-12px-row md:px-12px-row md:py-12px-col text-16px md:inline hidden text-[#545454]">
-          &quot;{advice.message}&quot;
+          <div
+            className={
+              'md:w-160px-row md:h-160px-col md:mt-105px-col md:ml-12px-row md:px-12px-row md:py-12px-col text-16px md:inline hidden text-[#545454] ' +
+              myFont.className
+            }
+          >
+            &quot;{advice.message}&quot;
           </div>
         </div>
       </div>
@@ -32,7 +45,11 @@ const StatisticsForm = () => {
         <h2 className="md:text-24px font-bold md:mb-12px-col mb-[8px] text-[18px] text-font-color">이달의 내 감정</h2>
         <EmotionChart />
       </div>
-      <div className="md:hidden w-[330px] h-[44px] text-[16px] mt-[24px] text-center text-[#545454]">&quot;{advice.message}&quot;</div>
+      <div
+        className={'md:hidden w-[330px] h-[44px] text-[16px] mt-[24px] text-center text-[#545454]' + myFont.className}
+      >
+        &quot;{advice.message}&quot;
+      </div>
     </div>
   );
 };
