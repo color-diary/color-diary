f90bdbd2f1d855c28827bb6f59fc90454b9e1afa
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-08-09 20:17:55 +0900

    add: useTypingWords hook

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import useTypingWords from '@/hooks/useTypingWords';
+import { useEffect } from 'react';
 import ReactLoading from 'react-loading';
 import Character from './assets/Character';
 
 const LoadingSpinner: React.FC = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  const typingText = useTypingWords('당신의 감정을 위한 공간을 준비중이에요! 잠시만 기다려주세요.');
+
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-20 flex flex-col items-center justify-center bg-layout gap-16px-col-m md:gap-16px-col">
       <div className="flex items-center justify-center">
@@ -23,9 +36,7 @@ const LoadingSpinner: React.FC = () => {
         </div>
         <Character />
       </div>
-      <p className="text-center text-14px-m md:text-20px font-normal tracking-0.28px md:tracking-tight">
-        당신의 감정을 위한 공간을 준비중이에요! 잠시만 기다려주세요.
-      </p>
+      <p className="text-start text-14px-m md:text-20px font-normal tracking-0.28px md:tracking-tight">{typingText}</p>
     </div>
   );
 };
---
+++ src/hooks/useTypingWords.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const useTypingWords = (text: string): string => {
+  const [typing, setTyping] = useState<string>('');
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const typingWords = setInterval(() => {
+      if (typing.length === text.length) return;
+
+      setTyping((prevTyping) => {
+        const nextTyping = prevTyping ? prevTyping + text[count] : text[0];
+        setCount(count + 1);
+
+        return nextTyping;
+      });
+    }, 100);
+
+    return () => clearInterval(typingWords);
+  });
+
+  return typing;
+};
+
+export default useTypingWords;
