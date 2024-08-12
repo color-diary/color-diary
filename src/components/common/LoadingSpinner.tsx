d516015cc25f6525c809266cace8de97ac11cc11
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-08-12 15:48:14 +0900

    update: loading spinner 문구 text-start로 나타나도록 수정

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -13,7 +13,8 @@ const LoadingSpinner: React.FC = () => {
     };
   }, []);
 
-  const typingText = useTypingWords('당신의 감정을 위한 공간을 준비중이에요! 잠시만 기다려주세요.');
+  const loadingText = '당신의 감정을 위한 공간을 준비중이에요! 잠시만 기다려주세요.';
+  const typingText = useTypingWords(loadingText);
 
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-20 flex flex-col items-center justify-center bg-layout gap-16px-col-m md:gap-16px-col">
@@ -31,7 +32,8 @@ const LoadingSpinner: React.FC = () => {
         <Character />
       </div>
       <div className="flex items-center">
-        <p className="text-start text-14px-m md:text-20px font-normal tracking-0.28px md:tracking-tight">
+        <div className="relative text-layout">{loadingText}</div>
+        <p className="absolute text-start text-14px-m md:text-20px font-normal tracking-0.28px md:tracking-tight">
           {typingText}
         </p>
       </div>
